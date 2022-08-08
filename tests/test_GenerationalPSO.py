d78ed842e32249055afb15efd4dd80ad75a2e013
qthequartermasterman <qthequartermasterman@gmail.com>
2022-08-07 20:54:47 -0500

    Implement Naive GenerationalPSO

---
+++ tests/test_GenerationalPSO.py
@@ -0,0 +1,73 @@
+from unittest import TestCase
+
+import torch
+
+from torch_pso import GenerationalPSO as ParticleSwarmOptimizer
+
+
+class SquareWeightsModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.weights = torch.nn.Parameter(torch.rand((1,)))
+
+    def forward(self, x):
+        x = self.weights
+        return x ** 2
+
+class QuarticWeightsModule(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.weights = torch.nn.Parameter(torch.rand((1,)))
+
+    def forward(self, x):
+        x = self.weights
+        return x**2 * (x**2-1)
+
+
+class TestParticleSwarmOptimizer(TestCase):
+    def test_square_converges(self):
+        self.net = SquareWeightsModule()
+        self.optim = ParticleSwarmOptimizer(self.net.parameters(),
+                                            inertial_weight=5e-2,
+                                            cognitive_coefficient=1e-2,
+                                            social_coefficient=1e-2,
+                                            num_particles=100,
+                                            max_param_value=1,
+                                            min_param_value=-1)
+        def closure():
+            self.optim.zero_grad()
+            return self.net(None)
+
+        for _ in range(1000):
+            self.optim.step(closure)
+
+        converged = False
+        for _ in range(1000):
+            self.optim.step(closure)
+            if torch.allclose(self.net.weights, torch.Tensor([0.]), atol=1e-4, rtol=1e-3):
+                converged = True
+                break
+
+        assert converged, self.net.weights
+
+    def test_quartic_converges(self):
+        self.net = QuarticWeightsModule()
+        self.optim = ParticleSwarmOptimizer(self.net.parameters(),
+                                            inertial_weight=5e-2,
+                                            cognitive_coefficient=1e-2,
+                                            social_coefficient=1e-2,
+                                            num_particles=100,
+                                            max_param_value=1,
+                                            min_param_value=-1)
+        def closure():
+            self.optim.zero_grad()
+            return self.net(None)
+
+        converged = False
+        for _ in range(1000):
+            self.optim.step(closure)
+            if torch.allclose(abs(self.net.weights), torch.sqrt(torch.Tensor([2]))/2, atol=1e-4, rtol=1e-3):
+                converged = True
+                break
+
+        assert converged, self.net.weights
---
+++ torch_pso/optim/GenerationalPSO.py
@@ -0,0 +1,59 @@
+import random
+from typing import Callable, Iterable
+
+import torch
+
+from .ParticleSwarmOptimizer import ParticleSwarmOptimizer, clone_param_group, clone_param_groups, Particle
+
+
+class GenerationalPSO(ParticleSwarmOptimizer):
+    def __init__(self,
+                 params: Iterable[torch.nn.Parameter],
+                 inertial_weight: float = .9,
+                 cognitive_coefficient: float = 1.,
+                 social_coefficient: float = 1.,
+                 num_particles: int = 100,
+                 max_param_value: float = 10.,
+                 min_param_value: float = -10.,
+                 generational_turnover_ratio: float = .3,
+                 keep_top_performers: int = 10):
+        super().__init__(params, inertial_weight, cognitive_coefficient, social_coefficient, num_particles,
+                         max_param_value, min_param_value)
+        self.generational_turnover_ratio = generational_turnover_ratio
+        self.keep_top_performers = keep_top_performers
+
+    @torch.no_grad()
+    def step(self, closure: Callable[[], torch.Tensor]) -> torch.Tensor:
+        """
+        Performs a single optimization step.
+
+        :param closure: A callable that reevaluates the model and returns the loss.
+        :return: the final loss after the step (as calculated by the closure)
+        """
+        losses = {}
+        for i, particle in enumerate(self.particles):
+            particle_loss = particle.step(closure, self.best_known_global_param_groups)
+            losses[i] = particle_loss
+            if particle_loss < self.best_known_global_loss_value:
+                self.best_known_global_param_groups = clone_param_groups(particle.position)
+                self.best_known_global_loss_value = particle_loss
+
+        # set the module's parameters to be the best performing ones
+        for master_group, best_group in zip(self.param_groups, self.best_known_global_param_groups):
+            clone = clone_param_group(best_group)['params']
+            for i in range(len(clone)):
+                master_group['params'][i].data = clone[i].data
+
+        # Respawn a certain proportion of the worst performing particles, chosen at random
+        best_performers_indices = list(sorted(losses, key=losses.get, reverse=True))
+        bottom_performers = best_performers_indices[self.keep_top_performers:]
+        indices_to_respawn = random.sample(bottom_performers, round(self.generational_turnover_ratio * len(losses)))
+        for index in indices_to_respawn:
+            self.particles[index] = Particle(self.param_groups,
+                                             self.inertial_weight,
+                                             self.cognitive_coefficient,
+                                             self.social_coefficient,
+                                             max_param_value=self.max_param_value,
+                                             min_param_value=self.min_param_value)
+
+        return closure()  # loss = closure()
